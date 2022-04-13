2bf6cf652e91398b9ec08996bf6a1dba86a11bc4
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-04-13 11:03:22 +0300

    Create config.js

---
+++ server/src/config/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+
+    development : {
+        PORT: 5000,
+        dbAtlas: 'mongodb+srv://niki:niki548444@cluster0.rw93m.mongodb.net/gift-vouchers?retryWrites=true&w=majority',
+        secret: 'mnogoqkaparola'
+    },
+
+    production: {
+        PORT: 80
+    }
+}
