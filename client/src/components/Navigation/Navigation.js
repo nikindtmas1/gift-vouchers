5552f023f0d60f596cff4a024e5018d1da2217f9
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-04-14 11:23:55 +0300

    Update navigation

--- client/src/components/Navigation/Navigation.js
+++ client/src/components/Navigation/Navigation.js
@@ -1,8 +1,39 @@
-import React from 'react'
+import * as React from 'react';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import Box from '@mui/material/Box';
+
+
+  
+  function LinkTab(props) {
+    return (
+      <Tab
+        component="a"
+        onClick={(event) => {
+          event.preventDefault();
+        }}
+        {...props}
+      />
+    );
+  }
 
 const Navigation = () => {
+
+    
+
+    //const [value, setValue] = React.useState('one');
+
+//   const handleChange = (event: React.SyntheticEvent, newValue: string) => {
+//     setValue(newValue);
+
   return (
-    <div>Navigation</div>
+    <Box sx={{ width: '100%' }}>
+      <Tabs  aria-label="nav tabs example">
+        <LinkTab label="Page One" href="/drafts" />
+        <LinkTab label="Page Two" href="/trash" />
+        <LinkTab label="Page Three" href="/spam" />
+      </Tabs>
+    </Box>
   )
 }
 
