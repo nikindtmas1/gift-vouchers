533a1011f0757e2e1f8b97c840604f4551de7733
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-04-14 09:41:22 +0300

    Change pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import CreatePage from "./components/CreatePage/CreatePage";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          <CreatePage />
       </header>
     </div>
   );
