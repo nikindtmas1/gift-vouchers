6a9e21b036a1a20536e9876f928c9cb6ba0726a4
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-04-14 10:44:27 +0300

    Import navigation and footer

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,16 @@
 import CreatePage from "./components/CreatePage/CreatePage";
+import Navigation from "./components/Navigation/Navigation";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+          <Navigation />
           <CreatePage />
       </header>
+      <div><br /></div>
+      <Footer />
     </div>
   );
 }
