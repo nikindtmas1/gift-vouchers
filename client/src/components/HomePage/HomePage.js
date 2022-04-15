daf12f11295679373636b3a5e7e93455e6cca06f
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-04-15 11:32:09 +0300

    Update HomePage.js

--- client/src/components/HomePage/HomePage.js
+++ client/src/components/HomePage/HomePage.js
@@ -4,7 +4,7 @@ const HomePage = () => {
   return (
     <>
       <div className="menu">
-        <div className="icon-close">
+        <div className="homeimg">
           <img src="https://media.istockphoto.com/photos/mountain-landscape-picture-id517188688?k=20&m=517188688&s=612x612&w=0&h=i38qBm2P-6V4vZVEaMy_TaTEaoCMkYhvLCysE7yJQ5Q=" />
         </div>
 
@@ -16,17 +16,33 @@ const HomePage = () => {
       </ul> */}
       </div>
 
-      <div className="jumbotron">
+      {/* <div className="jumbotron">
         <div className="icon-menu">
           <i className="fa fa-bars"></i>
         </div>
-      </div>
+      </div> */}
 
       <form className="form" action="#" autocomplete="off">
         <fieldset className="url">
           <input id="url" type="text" name="url" placeholder='Search voucher...' />
           {/* <label for="url"><i className="fa fa-search" aria-hidden="true"></i> Search</label> */}
-          <div className="after"></div>
+          {/* <div className="after"></div> */}
+        </fieldset>
+        <fieldset className="enterhome">
+          <button className='homebutton'>Find</button>
+        </fieldset>
+        <fieldset className="url">
+          <input id="url" type="text" name="url" placeholder='Search voucher...' />
+          {/* <label for="url"><i className="fa fa-search" aria-hidden="true"></i> Search</label> */}
+          {/* <div className="after"></div> */}
+        </fieldset>
+        <fieldset className="enterhome">
+          <button className='homebutton'>Find</button>
+        </fieldset>
+        <fieldset className="url">
+          <input id="url" type="text" name="url" placeholder='Search voucher...' />
+          {/* <label for="url"><i className="fa fa-search" aria-hidden="true"></i> Search</label> */}
+          {/* <div className="after"></div> */}
         </fieldset>
         <fieldset className="enterhome">
           <button className='homebutton'>Find</button>
