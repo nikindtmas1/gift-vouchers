09dc57601df8ba6e988509ab3d8e5869413ca7f3
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-04-17 16:28:05 +0300

    Update Home Page

--- client/src/components/HomePage/HomePage.js
+++ client/src/components/HomePage/HomePage.js
@@ -26,7 +26,7 @@ const HomePage = () => {
     <>
       <div className="menu">
         <div className="homeimg">
-          <img src="https://media.istockphoto.com/photos/mountain-landscape-picture-id517188688?k=20&m=517188688&s=612x612&w=0&h=i38qBm2P-6V4vZVEaMy_TaTEaoCMkYhvLCysE7yJQ5Q=" />
+          <img src="https://media.istockphoto.com/photos/mountain-landscape-picture-id517188688?k=20&m=517188688&s=612x612&w=0&h=i38qBm2P-6V4vZVEaMy_TaTEaoCMkYhvLCysE7yJQ5Q=" alt='' />
         </div>
 
         {/* <ul>
