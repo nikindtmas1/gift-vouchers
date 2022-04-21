4335fea3bb1d53bd98915a0aec99252e7509061b
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-04-21 16:08:56 +0300

    Update HomePage

--- client/src/components/HomePage/HomePage.js
+++ client/src/components/HomePage/HomePage.js
@@ -1,9 +1,45 @@
 import React from 'react';
+import { useContext } from 'react';
+import AuthCtx from '../../contexts/AuthCtx';
 
 
 const HomePage = () => {
 
-  
+  let value = useContext(AuthCtx);
+  let vouchers = value.vouchers;
+  console.log(vouchers);
+  let searchVoucher
+  //let voucherData
+  // const [searchVoucher, setSearchVoucher] = useState('');
+  // const [voucherData, setVoucherData] = useState({});
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    let formData = new FormData(e.currentTarget);
+    let numberVoucher = formData.get('numVoucher');
+    let ownerName = formData.get('ownName');
+
+    if(numberVoucher !== '' && numberVoucher !== undefined){
+        searchVoucher = numberVoucher;
+        console.log(searchVoucher);
+        let foundVoucher = vouchers.filter((x) => x.numVoucher === Number(searchVoucher));
+
+        console.log(foundVoucher);
+    };
+
+    if(ownerName !== '' && ownerName !== undefined){
+      searchVoucher = ownerName;
+      let foundVoucher = value.filter((x) => x.numVoucher === Number(searchVoucher));
+
+      console.log(foundVoucher);
+      
+    };
+
+    
+
+  }
+
 //   async function getAll(query){
 
 //     let results = await Cube.find({}).lean();
@@ -45,9 +81,9 @@ const HomePage = () => {
         </div>
       </div> */}
 
-      <form className="form" action="#" autocomplete="off">
+      <form onSubmit={onSubmit} className="form" action="#" autocomplete="off">
         <fieldset className="url">
-          <input id="url" type="text" name="url" placeholder='Search voucher...' />
+          <input id="url" type="text" name="numVoucher" placeholder='Search by number...' />
           {/* <label for="url"><i className="fa fa-search" aria-hidden="true"></i> Search</label> */}
           {/* <div className="after"></div> */}
         </fieldset>
@@ -55,21 +91,19 @@ const HomePage = () => {
           <button className='homebutton'>Find</button>
         </fieldset>
         <fieldset className="url">
-          <input id="url" type="text" name="url" placeholder='Search voucher...' />
+          <input id="url" type="text" name="ownName" placeholder='Search by Owner Name...' />
           {/* <label for="url"><i className="fa fa-search" aria-hidden="true"></i> Search</label> */}
           {/* <div className="after"></div> */}
         </fieldset>
         <fieldset className="enterhome">
           <button className='homebutton'>Find</button>
         </fieldset>
-        <fieldset className="url">
+        {/* <fieldset className="url">
           <input id="url" type="text" name="url" placeholder='Search voucher...' />
-          {/* <label for="url"><i className="fa fa-search" aria-hidden="true"></i> Search</label> */}
-          {/* <div className="after"></div> */}
         </fieldset>
         <fieldset className="enterhome">
           <button className='homebutton'>Find</button>
-        </fieldset>
+        </fieldset> */}
       </form>
     </>
   );
