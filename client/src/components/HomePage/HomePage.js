a80ca66d4e7da83f3e0dc520000e513628279eb0
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-04-22 18:40:47 +0300

    Update HomePage

--- client/src/components/HomePage/HomePage.js
+++ client/src/components/HomePage/HomePage.js
@@ -1,21 +1,22 @@
 import React from 'react';
 import { useContext } from 'react';
 import AuthCtx from '../../contexts/AuthCtx';
-import SearchPage from '../SearchPage/SearchPage';
+//import SearchPage from '../SearchPage/SearchPage';
 
 
 const HomePage = () => {
 
   let value = useContext(AuthCtx);
   let vouchers = value.vouchers;
-  console.log(vouchers);
-  let searchVoucher
+
+  let searchVoucher;
+ 
   //let voucherData
   // const [searchVoucher, setSearchVoucher] = useState('');
   // const [voucherData, setVoucherData] = useState({});
 
   const onSubmit = (e) => {
-    e.preventDefault();
+    // e.preventDefault();
 
     let formData = new FormData(e.currentTarget);
     let numberVoucher = formData.get('numVoucher');
@@ -27,6 +28,7 @@ const HomePage = () => {
       searchVoucher = foundVoucher;
 
       console.log(searchVoucher);
+     
         console.log(searchVoucher[0].email);
     }
     // if(ownerName !== '' && ownerName !== undefined){
@@ -106,7 +108,51 @@ const HomePage = () => {
           <button className='homebutton'>Find</button>
         </fieldset> */}
       </form>
-      <SearchPage data={{searchVoucher}} ></SearchPage>
+      {searchVoucher ?
+      <div className='searchDiv'>
+      <div className='searchcontainer'>
+          {/* <div className=" text-center mt-5 ">
+              <h1>Search Page</h1>
+          </div> */}
+          <form className='formSearch'>
+              <div className='controls'>
+              <div className='row'>
+                  <div className="col-md-6">
+                      <div className="form-group"> <label for="form_name">Date *</label> <input id="form_name" type="text" name="date" className="form-control"   required="required" data-error="Date is required." ></input> </div>
+                      <div className="form-group"> <label for="form_name">Number Voucher *</label> <input id="form_name" type="text" name="numberVoucher" className="form-control" placeholder="Enter Number Voucher *" required="required" data-error="Number Voucher is required." /> </div>
+                      <div className="form-group"> <label for="form_name">Name Buyer *</label> <input id="form_name" type="text" name="nameBuyer" className="form-control" placeholder="Enter Name Buyer *" required="required" data-error="Name Buyer is required." /> </div>
+                  </div>
+                  <div className="col-md-6">
+                      <div className="form-group"> <label for="form_lastname">Name Owner *</label> <input id="form_lastname" type="text" name="nameOwner" className="form-control" placeholder="Enter Name Owner *" required="required" data-error="Name Owner is required." /> </div>
+                      <div className="form-group"> <label for="form_lastname">Name Employee *</label> <input id="form_lastname" type="text" name="nameEmployee" className="form-control" placeholder="Enter Name Employee *" required="required" data-error="Name Employee is required." /> </div>
+                      <div className="form-group"> <label for="form_lastname">Treatment *</label> <input id="form_lastname" type="text" name="nameTreatment" className="form-control" placeholder="Enter Treatment *" required="required" data-error="Treatment is required." /> </div>
+                  </div>
+              </div>
+              <div className="row">
+                  <div className="col-md-6">
+                      <div className="form-group"> <label for="form_email">Count *</label> <input id="form_email" type="text" name="count" className="form-control" placeholder="Please enter Count *" required="required" data-error="Count is required." /> </div>
+                      <div className="form-group"> <label for="form_email">Price *</label> <input id="form_email" type="text" name="price" className="form-control" placeholder="Please enter Price *" required="required" data-error="Price is required." /> </div>
+                      <div className="form-group"> <label for="form_email">Valid Date *</label> <input id="form_email" type="text" name="validDate" className="form-control" placeholder="Enter valid date *" required="required" data-error="Valid date is required." /> </div>
+                  </div>
+                  <div className="col-md-6">
+                      <div className="form-group"> <label for="form_lastname">Type Transaction *</label> <input id="form_lastname" type="text" name="typeTransaction" className="form-control" placeholder="Enter Type Transaction *" required="required" data-error="Type Transaction is required." /> </div>
+                      <div className="form-group"> <label for="form_lastname">Used Date *</label> <input id="form_lastname" type="text" name="usedDate" className="form-control" placeholder="Please enter Used Date *" required="required" data-error="Used Date is required." /> </div>
+                      <div className="form-group"> <label for="form_lastname">Email *</label> <input id="form_lastname" type="email" name="email" className="form-control" placeholder="Please enter your email *" required="required" data-error="Valid email is required." /> </div>
+                  </div>
+              </div>
+              <div className="row">       
+                  <div className="col-md-12">
+                       <input type="submit" className="btnSearch" value="Edit" /> 
+                  </div>
+              </div>
+              
+              </div>
+          </form>
+      </div>
+  </div>
+  : null
+    }
+      
     </>
   );
 }
