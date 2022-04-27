2b8a9f6b0c5d52cfc66b93552629c711962542be
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-04-27 15:40:40 +0300

    Update LoginPage.js

--- client/src/components/LoginPage/LoginPage.js
+++ client/src/components/LoginPage/LoginPage.js
@@ -1,16 +1,46 @@
-import React from 'react'
+import React from 'react';
+
+import * as services from '../Services/data';
 
 const LoginPage = () => {
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    let formData = new FormData(e.currentTarget);
+
+    let username = formData.get('userName').trim();
+    let password = formData.get('userPassword').trim();
+
+    if (username === "" || password === "") {
+      return alert("All fields are required!");
+    }
+
+    if (username.length < 3) {
+      return alert("Username input is invalid!");
+    }
+
+    services.login(username, password)
+        .then(() => {
+          console.log('is ok');
+        })
+        .catch(error => console.log(error))
+        // history.push('/');
+
+  }
+
   return (
     <div className="containerlogin">
   {/* <div className="brand-logo"></div>
   <div className="brand-title">TWITTER</div> */}
   <div className="inputs">
+    <form onSubmit={onSubmit} method='POST'>
     <label>USER NAME</label>
-    <input type="text" placeholder="User name" />
+    <input type="text" name='userName' placeholder="User name" />
     <label>PASSWORD</label>
-    <input type="password" placeholder="Min 6 charaters long" />
+    <input type="password" name='userPassword' placeholder="Min 6 charaters long" />
     <button className='loginbutton' type="submit">LOGIN</button>
+    </form>
   </div>
   {/* <a href="https://twitter.com/prathkum">MADE BY PRATHAM</a> */}
 </div>
