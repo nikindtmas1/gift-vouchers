15fae0861905bc0017ca0c7e3dc0e59cdeb5d5ee
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-04-27 14:56:26 +0300

    Update routes.js

--- server/src/routes/routes.js
+++ server/src/routes/routes.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 
 const vouchersController = require('../controllers/voucherController');
+const userController = require('../controllers/userController');
 
+router.use('/users', userController);
 router.use('/vouchers', vouchersController);
 
 module.exports = router;
