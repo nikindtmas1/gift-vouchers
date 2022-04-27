bdac1f02c8686375cb25b9794f0169221046a495
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-04-27 14:42:42 +0300

    Added login logic

--- server/src/controllers/userController.js
+++ server/src/controllers/userController.js
@@ -1,3 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
+const services = require('../services/userServices');
+
+router.post('/login', async (req, res) => {
+    let {username, password} = req.body;
+
+    let {user} = await services.login({username, password});
+
+    res.json({user});
+});
+
+module.exports = router;
