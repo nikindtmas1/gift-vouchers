a57d36b57141fbcdbc68d3de15b074d0280427a4
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-04-27 14:43:03 +0300

    Added login logic

--- server/src/services/userServices.js
+++ server/src/services/userServices.js
@@ -1,1 +1,12 @@
 const User = require('../models/userModel');
+const jwt = require('jsonwebtoken');
+
+exports.login = async ({username, password}) => {
+
+    let user = await User.findOne({username, password});
+
+    if(user){
+
+        console.log(user);
+    }
+}
