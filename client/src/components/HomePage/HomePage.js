5f2909e2a61eb751e7a99c6f1849cb3a8e2ab636
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-04-27 12:03:55 +0300

    Added image

--- client/src/components/HomePage/HomePage.js
+++ client/src/components/HomePage/HomePage.js
@@ -86,7 +86,7 @@ const HomePage = () => {
     <>
       <div className="menu">
         <div className="homeimg">
-          <img src="https://media.istockphoto.com/photos/mountain-landscape-picture-id517188688?k=20&m=517188688&s=612x612&w=0&h=i38qBm2P-6V4vZVEaMy_TaTEaoCMkYhvLCysE7yJQ5Q=" alt='' />
+          <img src="https://www.thehungrypeacock.com.au/wp-content/uploads/2020/04/gift-voucher-3-scaled.jpg" alt='' />
         </div>
 
        
