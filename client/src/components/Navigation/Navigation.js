2df2f5177665dc4df1ec185e61266f801cfc4b0c
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-04-28 16:10:48 +0300

    Navigate by Auth

--- client/src/components/Navigation/Navigation.js
+++ client/src/components/Navigation/Navigation.js
@@ -1,11 +1,49 @@
-import * as React from 'react';
+import React, {useContext} from 'react';
 import {Link} from 'react-router-dom';
 
+import AuthCtx from '../../contexts/AuthCtx';
 
 const Navigation = () => {
 
-  return (
-    <ul className="navbar">
+  let value = useContext(AuthCtx);
+  //let user = value.user.user;
+  let isAuth = value.user.isAuthenticated;
+
+  let guest = (
+
+    <>
+      <div></div>
+      <div></div>
+      <div></div>
+      {/* <div className="navdiv">
+        <li className="navli">
+          <Link to="/home">Home Page</Link>
+        </li>
+      </div>
+      <div className="navdiv">
+        <li className="navli">
+          <Link to="/create">Create Page</Link>
+        </li>
+      </div> */}
+      <div className="navdiv">
+        <li className="navli">
+          <Link to="/login">Login</Link>
+        </li>
+      </div>
+      {/* <div className="navdiv">
+        <li className="navli">
+          <Link to="/logout">Logout</Link>
+        </li>
+      </div> */}
+      <div></div>
+      <div></div>
+      <div></div>
+    
+    </>
+  );
+
+  let userNav = (
+    <>
       <div></div>
       <div></div>
       <div></div>
@@ -19,11 +57,11 @@ const Navigation = () => {
           <Link to="/create">Create Page</Link>
         </li>
       </div>
-      <div className="navdiv">
+      {/* <div className="navdiv">
         <li className="navli">
           <Link to="/login">Login</Link>
         </li>
-      </div>
+      </div> */}
       <div className="navdiv">
         <li className="navli">
           <Link to="/logout">Logout</Link>
@@ -32,6 +70,15 @@ const Navigation = () => {
       <div></div>
       <div></div>
       <div></div>
+      </>
+  );
+
+  return (
+    <ul className="navbar">
+       {isAuth
+      ? userNav
+      : guest
+      }
     </ul>
   );
 }
