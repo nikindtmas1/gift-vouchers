3d01439c964d2763c6ab29c27011bcd17815fc52
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-04-28 13:33:54 +0300

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import {useState ,useEffect} from 'react';
 import AuthCtx from './contexts/AuthCtx';
 
 import * as services from './components/Services/data';
+import * as userServices from './components/Services/authService';
 
 import HomePage from './components/HomePage/HomePage';
 import CreatePage from "./components/CreatePage/CreatePage";
@@ -17,7 +18,8 @@ import Footer from "./components/Footer/Footer";
 
 function App() {
   
-  const [vouchers, setVaouchers] = useState([])
+  const [vouchers, setVaouchers] = useState([]);
+  const [userInfo, setUserInfo] = useState({ isAuthenticated: false, username: '', userId: '' });
 
   useEffect(() => {
 
@@ -26,9 +28,32 @@ function App() {
     .catch(err => alert(err.message))
   },[]);
 
+  useEffect(() => {
+    let userData = userServices.getUser();
+    let user = userData.username;
+    let id = userData.userId;
+
+
+    setUserInfo({
+      isAuthenticated: Boolean(user),
+      user: user,
+      userId: id,
+    })
+  }, []);
+
+  const onLogin = (userData) => {
+
+    let user = userData.username;
+    let id = userData._id;
+    setUserInfo({
+      user: user,
+      isAuthenticated: true,
+      userId: id,
+    })
+  };
  
   return (
-    <AuthCtx.Provider value={{vouchers}}>
+    <AuthCtx.Provider value={{vouchers, user: userInfo, onLogin}}>
     <div className="">
       <header className="">
           <Navigation />
