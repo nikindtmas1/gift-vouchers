053222ed3611cd606906a285a6d020e882837f54
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-04-28 13:33:39 +0300

    Create authService.js

---
+++ client/src/components/Services/authService.js
@@ -0,0 +1,9 @@
+export const getUser = () => {
+    let username = sessionStorage.getItem('username');
+    let userId = sessionStorage.getItem('userId');
+  
+    let userData = { username, userId }
+
+    //return username;
+    return userData;
+};
