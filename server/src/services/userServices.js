328f2318a9ac9002ec7b413ed7f5a7761c282be3
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-04-28 13:34:31 +0300

    Update userServices.js

--- server/src/services/userServices.js
+++ server/src/services/userServices.js
@@ -6,7 +6,31 @@ exports.login = async ({username, password}) => {
     let user = await User.findOne({username, password});
 
     if(user){
+        let accessToken = jwt.sign({_id: user._id, username: user.username}, 'MOGYSHTSECRET', { expiresIn: '1m' });
+        let refreshToken = await jwt.sign({ _id: user._id }, 'MOGYSHTSECRET2', { expiresIn: '1d' });
+        
+        user.refreshToken = refreshToken;
 
-        console.log(user);
+        await user.save();
+
+        return { user, accessToken, refreshToken };
+        
+    }else{
+        throw new Error('No such user');
     }
-}
+};
+
+exports.refresh = async (refreshToken) => {
+    let { _id } = jwt.verify(refreshToken, 'MOGYSHTSECRET2');
+
+    let user = await User.find({ _id, refreshToken });
+
+    if (user) {
+        let accessToken = jwt.sign({ _id: user._id, username: user.username }, 'MOGYSHTSECRET', { expiresIn: '1m' });
+        let refreshToken = jwt.sign({ _id: user._id }, 'MOGYSHTSECRET2', { expiresIn: '1d' });
+
+        return { accessToken, refreshToken };
+    } else {
+        return null;
+    }
+};
