1ddaf5798bebc556a5ec4b488d9b275408caf615
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-04-28 13:34:10 +0300

    Update userController.js

--- server/src/controllers/userController.js
+++ server/src/controllers/userController.js
@@ -6,9 +6,26 @@ const services = require('../services/userServices');
 router.post('/login', async (req, res) => {
     let {username, password} = req.body;
 
-    let user = await services.login({username, password});
+    let { user, accessToken, refreshToken } = await services.login({username, password});
 
-    res.json({user});
+    res.json({
+        _id: user._id,
+        username: user.username,
+        accessToken,
+        refreshToken,
+    });
 });
+
+router.post('/refresh', async (req, res) => {
+   
+    let refreshToken = req.body.refreshToken;
+  
+    let { accessToken, refreshToken: newRefreshToken } = await services.refresh(refreshToken);
+  
+    res.json({
+        accessToken,
+        refreshToken: newRefreshToken,
+    });
+  });
 
 module.exports = router;
