1a2c249156bed6957507568d094ff4c2d1164d9d
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-04-29 12:00:25 +0300

    Update userController

--- server/src/controllers/userController.js
+++ server/src/controllers/userController.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 
 const services = require('../services/userServices');
+const auth = require('../middleware/authMidd');
+const roles = require('../middleware/roles');
 
 router.post('/login', async (req, res) => {
     let {username, password} = req.body;
