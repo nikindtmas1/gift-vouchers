2706ec6d3b85fc4445cd2aca1f19653496cd5a49
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-04-29 10:54:06 +0300

    Create Middleware

---
+++ server/src/middleware/authMidd.js
@@ -0,0 +1,21 @@
+const jwt = require('jsonwebtoken');
+
+module.exports = (req, res, next) => {
+    const token = req.header('x-auth-token');
+    if(!token) return res.status(401).send({
+        ok: true,
+        error: 'Access denied. No token provided'
+    })
+
+    console.log(token);
+
+    // try {
+    //     const decoded = jwt.verify(token, )
+        
+    // } catch (error) {
+    //     return res.status(401).send({
+    //         ok: false,
+    //         error: "Token expired"
+    //     });
+    // }
+}
