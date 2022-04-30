9ed37e7cda058514656a6d6b98d8b5499656bbb1
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-04-30 13:07:18 +0300

    Update voucher model

--- server/src/models/voucherModel.js
+++ server/src/models/voucherModel.js
@@ -2,19 +2,55 @@ const mongoose = require('mongoose');
 
 const voucherSchema = new mongoose.Schema({
 
-    date: String,
-    numVoucher: Number,
-    nameBuyer: String,
-    nameOwn: String,
-    nameEmployee: String,
-    treatment: String,
-    count: Number,
-    price: Number,
-    validDate: String,
-    typeTransaction: String,
-    usedDate: String,
+    date: {
+        type: String,
+        required: [true, 'Date is required!']
+    },
+    numVoucher: {
+        type: Number,
+        required: [true, 'Number Voucher is required!']
+    },
+    nameBuyer: {
+        type: String,
+        required: [true, 'Name Buyer is required!']
+    },
+    nameOwn: {
+        type: String,
+        required: [true, 'Name Owner is required!']
+    },
+    nameEmployee: {
+        type: String,
+        required: [true, 'Name Employee is required!']
+    },
+    treatment: {
+        type: String,
+        required: [true, 'Treatment is required!']
+    },
+    count: {
+        type: Number,
+        required: [true, 'Count is required!']
+    },
+    price: {
+        type: Number,
+        required: [true, 'Price is required!']
+    },
+    validDate: {
+        type: String,
+        required: [true, 'Valid Date is required!']
+    },
+    typeTransaction: {
+        type: String,
+        required: [true, 'Type transaction is required!']
+    },
+    usedDate: {
+        type: String,
+        required: [true, 'User Date is required!']
+    },
     checked: Boolean,
-    email: String
+    email: {
+        type: String,
+        required: [true, 'Email is required!']
+    }
 });
 
 module.exports = mongoose.model('Voucher', voucherSchema);
