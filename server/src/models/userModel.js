5c1a18fe40641865e839e2c983f1b2da3454b179
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-04-30 13:07:05 +0300

    Update user model

--- server/src/models/userModel.js
+++ server/src/models/userModel.js
@@ -1,8 +1,14 @@
 const mongoose = require('mongoose');
 
 const userModel = new mongoose.Schema({
-    username: String,
-    password: String
+    username: {
+        type: String,
+        required: [true, 'Username is required!']
+    },
+    password: {
+        type: String,
+        required: [true, 'Password is required!']
+    }
 });
 
 module.exports = mongoose.model('User', userModel);
