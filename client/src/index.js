bf7612c7f8910fc6393bbf0d18ea1dc823e01cc9
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-05-09 12:23:32 +0300

    Update index

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,20 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
+//const container = document.getElementById('root');
+
+// create a root
+//const root = ReactDOM.createRoot(container);
+// const root = ReactDOM.createRoot(container);
+
+// root.render(
+//   <React.StrictMode>
+//     <BrowserRouter>
+//       <App />
+//     </BrowserRouter>
+//   </React.StrictMode>,
+// );
+
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
