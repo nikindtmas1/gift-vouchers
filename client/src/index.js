e261fdb899c70cd8cb5f9ae8adce0fff5d750026
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-05-09 15:19:06 +0300

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -10,7 +10,7 @@ const container = document.getElementById("root");
 const root = ReactDOM.createRoot(container);
 
 root.render(
-  // <React.StrictMode></React.StrictMode>,
+  
     <BrowserRouter>
       <App />
     </BrowserRouter>
