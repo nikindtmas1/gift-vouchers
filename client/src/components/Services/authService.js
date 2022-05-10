aaa26bfc0c8b17612b7d31546718c7f1c21a2d04
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-05-10 16:12:23 +0300

    Update Auth Service

--- client/src/components/Services/authService.js
+++ client/src/components/Services/authService.js
@@ -1,9 +1,9 @@
 export const getUser = () => {
     let username = sessionStorage.getItem('username');
     let userId = sessionStorage.getItem('userId');
-   
+    let userRoles = sessionStorage.getItem('userRoles'); 
   
-    let userData = { username, userId }
+    let userData = { username, userId, userRoles }
 
     //return username;
     return userData;
