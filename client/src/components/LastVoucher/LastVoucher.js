174d2e94c9ff2b533e295318906b66a030451e3b
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-05-12 11:12:44 +0300

    Added voucher data in LastVoucher.js

--- client/src/components/LastVoucher/LastVoucher.js
+++ client/src/components/LastVoucher/LastVoucher.js
@@ -13,73 +13,62 @@ const LastVoucher = () => {
 
     services.getAll()
     .then((result) => setVaouchers(result))
-    .catch(err => alert(err.message))
-    // const foundVoucher = vouchers.filter((x) => x.numVoucher === vouchers.length)
-    // setSearchVoucher(foundVoucher)
-    
+    .catch(err => alert(err.message))  
     },[null]);
 
-    useEffect(() => {
+    
+    const onClick = (e) => {
+        e.preventDefault();
+
         const foundVoucher = vouchers.filter((x) => x.numVoucher === vouchers.length)
         setSearchVoucher(foundVoucher)
-        setIsVoucher(true)
-    },[null]);
-    //console.log(searchVoucher[0].date);
-    
-    console.log(vouchers.length);
-    console.log(searchVoucher);
-    //const foundVoucher = vouchers.filter((x) => x.numVoucher === vouchers.length)
-    //console.log(foundVoucher[0].date);
 
-    // if(foundVoucher){
-    //     console.log('ok');
-    //     setSearchVoucher(foundVoucher[0]);
-    //     setIsVoucher(true);
-    // }
+        setIsVoucher(true)
+        
+    }
 
   return (
       <div className='searchDiv'>
+            <div className="row" onClick={onClick}>       
+                        <div className="col-md-12">
+                             <input type="submit" className="btnSearch" value="Find" /> 
+                        </div>
+                    </div>
       {isVoucher ?
         <div className='searchcontainer'>
-            {/* <div className=" text-center mt-5 ">
-                <h1>Search Page</h1>
-            </div> */}
-            <form className='formSearch'>
+               
+            <form  className='formSearch'>
+       
                 <div className='controls'>
+                      
                 <div className='row'>
                     <div className="col-md-6">
-                        <div className="form-group"> <label for="form_name">Date *</label> <input id="form_name" type="text" name="date" className="form-control"  required="required" data-error="Date is required." defaultValue={'ok'} ></input> </div>
-                        <div className="form-group"> <label for="form_name">Number Voucher *</label> <input id="form_name" type="text" name="numberVoucher" className="form-control" placeholder="Enter Number Voucher *" required="required" data-error="Number Voucher is required." /> </div>
-                        <div className="form-group"> <label for="form_name">Name Buyer *</label> <input id="form_name" type="text" name="nameBuyer" className="form-control" placeholder="Enter Name Buyer *" required="required" data-error="Name Buyer is required." /> </div>
+                        <div className="form-group"> <label for="form_name">Date *</label> <input id="form_name" type="text" name="date" className="form-control"  required="required" data-error="Date is required." defaultValue={searchVoucher[0].date} ></input> </div>
+                        <div className="form-group"> <label for="form_name">Number Voucher *</label> <input id="form_name" type="text" name="numberVoucher" className="form-control" placeholder="Enter Number Voucher *" required="required" data-error="Number Voucher is required." defaultValue={searchVoucher[0].numVoucher} /> </div>
+                        <div className="form-group"> <label for="form_name">Name Buyer *</label> <input id="form_name" type="text" name="nameBuyer" className="form-control" placeholder="Enter Name Buyer *" required="required" data-error="Name Buyer is required." defaultValue={searchVoucher[0].nameBuyer} /> </div>
                     </div>
                     <div className="col-md-6">
-                        <div className="form-group"> <label for="form_lastname">Name Owner *</label> <input id="form_lastname" type="text" name="nameOwner" className="form-control" placeholder="Enter Name Owner *" required="required" data-error="Name Owner is required." /> </div>
-                        <div className="form-group"> <label for="form_lastname">Name Employee *</label> <input id="form_lastname" type="text" name="nameEmployee" className="form-control" placeholder="Enter Name Employee *" required="required" data-error="Name Employee is required." /> </div>
-                        <div className="form-group"> <label for="form_lastname">Treatment *</label> <input id="form_lastname" type="text" name="nameTreatment" className="form-control" placeholder="Enter Treatment *" required="required" data-error="Treatment is required." /> </div>
+                        <div className="form-group"> <label for="form_lastname">Name Owner *</label> <input id="form_lastname" type="text" name="nameOwner" className="form-control" placeholder="Enter Name Owner *" required="required" data-error="Name Owner is required." defaultValue={searchVoucher[0].nameOwn} /> </div>
+                        <div className="form-group"> <label for="form_lastname">Name Employee *</label> <input id="form_lastname" type="text" name="nameEmployee" className="form-control" placeholder="Enter Name Employee *" required="required" data-error="Name Employee is required." defaultValue={searchVoucher[0].nameEmployee} /> </div>
+                        <div className="form-group"> <label for="form_lastname">Treatment *</label> <input id="form_lastname" type="text" name="nameTreatment" className="form-control" placeholder="Enter Treatment *" required="required" data-error="Treatment is required." defaultValue={searchVoucher[0].treatment} /> </div>
                     </div>
                 </div>
                 <div className="row">
                     <div className="col-md-6">
-                        <div className="form-group"> <label for="form_email">Count *</label> <input id="form_email" type="text" name="count" className="form-control" placeholder="Please enter Count *" required="required" data-error="Count is required." /> </div>
-                        <div className="form-group"> <label for="form_email">Price *</label> <input id="form_email" type="text" name="price" className="form-control" placeholder="Please enter Price *" required="required" data-error="Price is required." /> </div>
-                        <div className="form-group"> <label for="form_email">Valid Date *</label> <input id="form_email" type="text" name="validDate" className="form-control" placeholder="Enter valid date *" required="required" data-error="Valid date is required." /> </div>
+                        <div className="form-group"> <label for="form_email">Count *</label> <input id="form_email" type="text" name="count" className="form-control" placeholder="Please enter Count *" required="required" data-error="Count is required." defaultValue={searchVoucher[0].count} /> </div>
+                        <div className="form-group"> <label for="form_email">Price *</label> <input id="form_email" type="text" name="price" className="form-control" placeholder="Please enter Price *" required="required" data-error="Price is required." defaultValue={searchVoucher[0].price} /> </div>
+                        <div className="form-group"> <label for="form_email">Valid Date *</label> <input id="form_email" type="text" name="validDate" className="form-control" placeholder="Enter valid date *" required="required" data-error="Valid date is required." defaultValue={searchVoucher[0].validDate} /> </div>
                     </div>
                     <div className="col-md-6">
-                        <div className="form-group"> <label for="form_lastname">Type Transaction *</label> <input id="form_lastname" type="text" name="typeTransaction" className="form-control" placeholder="Enter Type Transaction *" required="required" data-error="Type Transaction is required." /> </div>
-                        <div className="form-group"> <label for="form_lastname">Used Date *</label> <input id="form_lastname" type="text" name="usedDate" className="form-control" placeholder="Please enter Used Date *" required="required" data-error="Used Date is required." /> </div>
-                        <div className="form-group"> <label for="form_lastname">Email *</label> <input id="form_lastname" type="email" name="email" className="form-control" placeholder="Please enter your email *" required="required" data-error="Valid email is required." /> </div>
+                        <div className="form-group"> <label for="form_lastname">Type Transaction *</label> <input id="form_lastname" type="text" name="typeTransaction" className="form-control" placeholder="Enter Type Transaction *" required="required" data-error="Type Transaction is required." defaultValue={searchVoucher[0].typeTransaction}/> </div>
+                        <div className="form-group"> <label for="form_lastname">Used Date *</label> <input id="form_lastname" type="text" name="usedDate" className="form-control" placeholder="Please enter Used Date *" required="required" data-error="Used Date is required." defaultValue={searchVoucher[0].usedDate} /> </div>
+                        <div className="form-group"> <label for="form_lastname">Email *</label> <input id="form_lastname" type="email" name="email" className="form-control" placeholder="Please enter your email *" required="required" data-error="Valid email is required." defaultValue={searchVoucher[0].email} /> </div>
                     </div>
-                </div>
-                {/* <div className="row">       
-                    <div className="col-md-12">
-                         <input type="submit" className="btnSearch" value="Edit" /> 
-                    </div>
-                </div> */}
-                
+                </div>   
                 </div>
             </form>
         </div>
-    :null}
+        :null}
     </div>
    
   )
