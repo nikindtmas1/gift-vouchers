01b1a1efcbfc24372360499f63a2d56c27a9f7b8
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-05-12 12:00:59 +0300

    Added router delete voucher

--- server/src/controllers/voucherController.js
+++ server/src/controllers/voucherController.js
@@ -41,4 +41,9 @@ router.put('/:id', async (req, res) => {
     res.json({ok: true});
 });
 
+router.delete('/:id', async (req, res) => {
+    await voucherService.deleteVoucher(req.params.id);
+    res.json({ok: true});
+});
+
 module.exports = router;
