eaf83c9622394ba90004b4ad553b3e05a3acfb6a
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-05-16 09:16:32 +0300

    Added history to register page

--- client/src/components/Register/RegisterPage.js
+++ client/src/components/Register/RegisterPage.js
@@ -1,33 +1,42 @@
 import React from 'react';
+import {useHistory} from 'react-router-dom';
 
 import * as services from '../Services/data';
 
-const onSubmit = async (e) => {
 
-  e.preventDefault();
-  let formData = new FormData(e.currentTarget);
-  let username = formData.get('userName').trim();
-  let password = formData.get('userPassword').trim();
-  let roles = formData.get('userRoles').trim();
-
-  if (username === "" || password === "" || roles === "") {
-    return alert("All fields are required!");
-  };
-
-  if (username.length < 3) {
-    return alert("Username input is invalid!");
-  };
-
-  try {
-   await services.register(username, password, roles)
-  } catch (error) {
-    alert(error.message);
-    throw error;
-  }
-
-};
 
 const RegisterPage = () => {
+  
+  const history = useHistory();
+
+  const onSubmit = (e) => {
+
+    e.preventDefault();
+  
+    let formData = new FormData(e.currentTarget);
+    let username = formData.get('userName').trim();
+    let password = formData.get('userPassword').trim();
+    let roles = formData.get('userRoles').trim();
+  
+    if (username === "" || password === "" || roles === "") {
+      return alert("All fields are required!");
+    };
+  
+    if (username.length < 3) {
+      return alert("Username input is invalid!");
+    };
+  
+    try {
+    services.register(username, password, roles)
+    .then(() => history.push('/login'))
+    } catch (error) {
+      alert(error.message);
+      throw error;
+    }
+  
+  };
+
+
   return (
     <div className="containerlogin">
     {/* <div className="brand-logo"></div>
