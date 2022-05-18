ccd462525b05060500217d579a6b2531458e7490
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-05-18 08:22:44 +0300

    Added table

--- client/src/components/CatalogPage/CatalogPage.js
+++ client/src/components/CatalogPage/CatalogPage.js
@@ -2,7 +2,25 @@ import React from 'react';
 
 const CatalogPage = () => {
   return (
-    <div>CatalogPage</div>
+    <div>
+      <table>
+  <tr>
+    <th>Company</th>
+    <th>Contact</th>
+    <th>Country</th>
+  </tr>
+  <tr>
+    <td>Alfreds Futterkiste</td>
+    <td>Maria Anders</td>
+    <td>Germany</td>
+  </tr>
+  <tr>
+    <td>Centro comercial Moctezuma</td>
+    <td>Francisco Chang</td>
+    <td>Mexico</td>
+  </tr>
+</table>
+    </div>
   )
 }
 
