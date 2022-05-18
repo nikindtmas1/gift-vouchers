58a5a5ffaecfdd564ea2f5b822bbd954dae7cfea
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-05-18 09:07:11 +0300

    Update CatalogPage

--- client/src/components/CatalogPage/CatalogPage.js
+++ client/src/components/CatalogPage/CatalogPage.js
@@ -1,27 +1,39 @@
-import React from 'react';
+import React from "react";
 
 const CatalogPage = () => {
   return (
     <div>
       <table>
-  <tr>
-    <th>Company</th>
-    <th>Contact</th>
-    <th>Country</th>
-  </tr>
-  <tr>
-    <td>Alfreds Futterkiste</td>
-    <td>Maria Anders</td>
-    <td>Germany</td>
-  </tr>
-  <tr>
-    <td>Centro comercial Moctezuma</td>
-    <td>Francisco Chang</td>
-    <td>Mexico</td>
-  </tr>
-</table>
+        <tr>
+          <th>Check</th>
+          <th>Date</th>
+          <th>Number Voucher</th>
+          <th>Name Buyer</th>
+          <th>Name Owner</th>
+          <th>Name Employee</th>
+          <th>Treatment</th>
+          <th>Count</th>
+          <th>Price</th>
+          <th>Valid Date</th>
+          <th>Type Transaction</th>
+          <th>Used Date</th>
+          <th>Email</th>
+        </tr>
+        <tr>
+          <td><input type="checkbox" /></td>
+          <td>Alfreds Futterkiste</td>
+          <td>Maria Anders</td>
+          <td>Germany</td>
+        </tr>
+        <tr>
+          <td><input type="checkbox" /></td>
+          <td>Centro comercial Moctezuma</td>
+          <td>Francisco Chang</td>
+          <td>Mexico</td>
+        </tr>
+      </table>
     </div>
-  )
-}
+  );
+};
 
-export default CatalogPage
+export default CatalogPage;
