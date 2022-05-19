ecffd5a7f6ca6dc8083a8906c848df6ad8b96249
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-05-19 09:47:47 +0300

    Format page

--- client/src/components/Logout/Logout.js
+++ client/src/components/Logout/Logout.js
@@ -1,23 +1,21 @@
-import {useContext,useEffect} from 'react';
-import {useHistory} from 'react-router-dom';
+import { useContext, useEffect } from "react";
+import { useHistory } from "react-router-dom";
 
-import * as authService from '../Services/data';
-import AuthCtx from '../../contexts/AuthCtx';
+import * as authService from "../Services/data";
+import AuthCtx from "../../contexts/AuthCtx";
 
 const Logout = () => {
-
-  let {onLogout} = useContext(AuthCtx);
+  let { onLogout } = useContext(AuthCtx);
 
   let history = useHistory();
 
   useEffect(() => {
-      authService.logout()
-        .then(() => {
-          onLogout()
-          history.push('/')
-        })
-    }, []);
-    return null;
+    authService.logout().then(() => {
+      onLogout();
+      history.push("/");
+    });
+  }, []);
+  return null;
 };
 
 export default Logout;
