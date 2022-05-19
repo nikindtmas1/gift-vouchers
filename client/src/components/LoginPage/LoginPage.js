6dc966db0657a16e6eeda7238a14017ce6005066
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-05-19 09:47:41 +0300

    Format page

--- client/src/components/LoginPage/LoginPage.js
+++ client/src/components/LoginPage/LoginPage.js
@@ -1,12 +1,11 @@
-import React, {useContext} from 'react';
-import {useHistory} from 'react-router-dom';
+import React, { useContext } from "react";
+import { useHistory } from "react-router-dom";
 
-import * as services from '../Services/data';
-import AuthCtx from '../../contexts/AuthCtx';
+import * as services from "../Services/data";
+import AuthCtx from "../../contexts/AuthCtx";
 
 const LoginPage = () => {
-
-  const {onLogin} = useContext(AuthCtx);
+  const { onLogin } = useContext(AuthCtx);
   const history = useHistory();
 
   const onSubmit = (e) => {
@@ -14,8 +13,8 @@ const LoginPage = () => {
 
     let formData = new FormData(e.currentTarget);
 
-    let username = formData.get('userName').trim();
-    let password = formData.get('userPassword').trim();
+    let username = formData.get("userName").trim();
+    let password = formData.get("userPassword").trim();
 
     if (username === "" || password === "") {
       return alert("All fields are required!");
@@ -25,31 +24,37 @@ const LoginPage = () => {
       return alert("Username input is invalid!");
     }
 
-    services.login(username, password)
-        .then(logData => {
-          onLogin(logData);
-        })
-        .catch(error => console.log(error))
-        history.push('/home');
-
-  }
+    services
+      .login(username, password)
+      .then((logData) => {
+        onLogin(logData);
+      })
+      .catch((error) => console.log(error));
+    history.push("/home");
+  };
 
   return (
     <div className="containerlogin">
-  {/* <div className="brand-logo"></div>
+      {/* <div className="brand-logo"></div>
   <div className="brand-title">TWITTER</div> */}
-  <div className="inputs">
-    <form onSubmit={onSubmit} method='POST'>
-    <label>USER NAME</label>
-    <input type="text" name='userName' placeholder="User name" />
-    <label>PASSWORD</label>
-    <input type="password" name='userPassword' placeholder="Min 6 charaters long" />
-    <button className='loginbutton' type="submit">LOGIN</button>
-    </form>
-  </div>
-  {/* <a href="https://twitter.com/prathkum">MADE BY PRATHAM</a> */}
-</div>
-  )
-}
+      <div className="inputs">
+        <form onSubmit={onSubmit} method="POST">
+          <label>USER NAME</label>
+          <input type="text" name="userName" placeholder="User name" />
+          <label>PASSWORD</label>
+          <input
+            type="password"
+            name="userPassword"
+            placeholder="Min 6 charaters long"
+          />
+          <button className="loginbutton" type="submit">
+            LOGIN
+          </button>
+        </form>
+      </div>
+      {/* <a href="https://twitter.com/prathkum">MADE BY PRATHAM</a> */}
+    </div>
+  );
+};
 
-export default LoginPage
+export default LoginPage;
