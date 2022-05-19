5371c9f3fe7cbccea53b0d731e4fe4d2e1d016c3
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-05-19 09:48:00 +0300

    Format page

--- client/src/components/Register/RegisterPage.js
+++ client/src/components/Register/RegisterPage.js
@@ -1,65 +1,65 @@
-import React from 'react';
-import {useHistory} from 'react-router-dom';
+import React from "react";
+import { useHistory } from "react-router-dom";
 
-import * as services from '../Services/data';
-
-
+import * as services from "../Services/data";
 
 const RegisterPage = () => {
-  
   const history = useHistory();
 
-
   const onSubmit = (e) => {
-
     e.preventDefault();
-  
+
     let formData = new FormData(e.currentTarget);
-    let username = formData.get('userName').trim();
-    let password = formData.get('userPassword').trim();
-    let roles = formData.get('userRoles').trim().toLowerCase();
-  
+    let username = formData.get("userName").trim();
+    let password = formData.get("userPassword").trim();
+    let roles = formData.get("userRoles").trim().toLowerCase();
+
     if (username === "" || password === "" || roles === "") {
       return alert("All fields are required!");
-    };
-  
+    }
+
     if (username.length < 3) {
       return alert("Username input is invalid!");
-    };
-
-    if (roles === "admin"){
-      return alert("The role can be only 'Editor' or 'Visitor'!")
     }
-  
+
+    if (roles === "admin") {
+      return alert("The role can be only 'Editor' or 'Visitor'!");
+    }
+
     try {
-    services.register(username, password, roles)
-    .then(() => history.push('/login'))
+      services
+        .register(username, password, roles)
+        .then(() => history.push("/login"));
     } catch (error) {
       alert(error.message);
       throw error;
     }
-  
   };
-
 
   return (
     <div className="containerlogin">
-    {/* <div className="brand-logo"></div>
+      {/* <div className="brand-logo"></div>
     <div className="brand-title">TWITTER</div> */}
-    <div className="inputs">
-      <form onSubmit={onSubmit} method='POST'>
-      <label>USER NAME</label>
-      <input type="text" name='userName' placeholder="User name" />
-      <label>PASSWORD</label>
-      <input type="password" name='userPassword' placeholder="Min 6 charaters long" />
-      <label>ROLE</label>
-      <input type="text" name='userRoles' placeholder="Add role" />
-      <button className='loginbutton' type="submit">REGISTER</button>
-      </form>
+      <div className="inputs">
+        <form onSubmit={onSubmit} method="POST">
+          <label>USER NAME</label>
+          <input type="text" name="userName" placeholder="User name" />
+          <label>PASSWORD</label>
+          <input
+            type="password"
+            name="userPassword"
+            placeholder="Min 6 charaters long"
+          />
+          <label>ROLE</label>
+          <input type="text" name="userRoles" placeholder="Add role" />
+          <button className="loginbutton" type="submit">
+            REGISTER
+          </button>
+        </form>
+      </div>
+      {/* <a href="https://twitter.com/prathkum">MADE BY PRATHAM</a> */}
     </div>
-    {/* <a href="https://twitter.com/prathkum">MADE BY PRATHAM</a> */}
-  </div>
-  )
-}
+  );
+};
 
-export default RegisterPage
+export default RegisterPage;
