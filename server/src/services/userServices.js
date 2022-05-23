a70a5fe5f465c6714135ebc22069e0966f0ffa76
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-05-23 09:33:02 +0300

    Update userService

--- server/src/services/userServices.js
+++ server/src/services/userServices.js
@@ -1,99 +1,95 @@
-const User = require('../models/userModel');
-const jwt = require('jsonwebtoken');
-const bcrypt = require('bcrypt');
-const { createAccessToken, createRefreshToken } = require('../utils/jwtUtils');
+const User = require("../models/userModel");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const { createAccessToken, createRefreshToken } = require("../utils/jwtUtils");
 
 // function register({username, password, roles}){
 
 //     let user = new User({username, password, roles})
-    
+
 //     return user.save();
 //     }
 
-exports.register = ({username, password, roles}) => {
-   
-        const saltPass = 9;
-        const myPass = password;
-       
-        bcrypt.genSalt(saltPass, (err, salt) => {
-            bcrypt.hash(myPass, salt, (err, hash) => {
-               
-                let password = hash;
-                let user = new User({username, password, roles})
-                user.save();
-               
-            });
-        });
-    // let user = new User({username, hashPass, roles})
-    // await user.save();
-    //User.create({username, password, roles});
+exports.register = ({ username, password, roles }) => {
+  const saltPass = 9;
+  const myPass = password;
+
+  bcrypt.genSalt(saltPass, (err, salt) => {
+    bcrypt.hash(myPass, salt, (err, hash) => {
+      let password = hash;
+      let user = new User({ username, password, roles });
+      user.save();
+    });
+  });
+  // let user = new User({username, hashPass, roles})
+  // await user.save();
+  //User.create({username, password, roles});
 };
 
-exports.login = async ({username, password}) => {
+exports.login = async ({ username, password }) => {
+  const currUser = username;
+  const currPass = password;
 
-    const currUser = username;
-    const currPass = password;
+  const user = await User.findByUsername(currUser);
 
-    const user = await User.findByUsername(currUser);
-   
-    if(!user) throw new Error('Invalid username!')
-    const valide = await bcrypt.compare(currPass, user.password);
-    if(!valide) throw new Error('Invalid password!');
-    //let user = await User.findOne({username, password});
-  
-    if(user.username === currUser){
-        
-        // const saltPass = 9;
-        // const myPass = 'miki123456';
-        // bcrypt.genSalt(saltPass, (err, salt) => {
-        //     bcrypt.hash(myPass, salt, (err, hash) => {
-        //         console.log(hash)
-               
-        //     });
-        // });
-        //const hashPass = '$2b$09$Lk7bxzhn0bTUEWRUxu9Q8ODbrQjipzpgQlSv88VUSJO5PJESpDn4.'
-        //const secHash = '$2b$09$L.Qotph6f7plVzOERix7IuwjJYdaXVytQzBNYU53NH6E5.RaTiOFq'
-            const accessToken = createAccessToken(user);
-            //const refreshToken = createRefreshToken(user);
-            //const accessToken = jwt.sign({_id: user._id, username: user.username}, 'MOGYSHTSECRET', { expiresIn: '60m' });
-            const refreshToken = await jwt.sign({ _id: user._id }, 'MOGYSHTSECRET2', { expiresIn: '1d' });
-            
-            user.refreshToken = refreshToken;
-            const userRoles = user.roles;
-            user.roles = userRoles;
-    
-            await user.save();
-    
-            return { user, accessToken, refreshToken, userRoles };
-        
+  if (!user) throw new Error("Invalid username!");
+  const valide = await bcrypt.compare(currPass, user.password);
+  if (!valide) throw new Error("Invalid password!");
+  //let user = await User.findOne({username, password});
 
-        // let accessToken = jwt.sign({_id: user._id, username: user.username, roles: user.roles}, 'MOGYSHTSECRET', { expiresIn: '1m' });
-        // let refreshToken = await jwt.sign({ _id: user._id }, 'MOGYSHTSECRET2', { expiresIn: '1d' });
-        
-        // user.refreshToken = refreshToken;
+  if (user.username === currUser) {
+    // const saltPass = 9;
+    // const myPass = 'miki123456';
+    // bcrypt.genSalt(saltPass, (err, salt) => {
+    //     bcrypt.hash(myPass, salt, (err, hash) => {
+    //         console.log(hash)
 
-        // await user.save();
+    //     });
+    // });
+    //const hashPass = '$2b$09$Lk7bxzhn0bTUEWRUxu9Q8ODbrQjipzpgQlSv88VUSJO5PJESpDn4.'
+    //const secHash = '$2b$09$L.Qotph6f7plVzOERix7IuwjJYdaXVytQzBNYU53NH6E5.RaTiOFq'
+    const accessToken = createAccessToken(user);
+    //const refreshToken = createRefreshToken(user);
+    //const accessToken = jwt.sign({_id: user._id, username: user.username}, 'MOGYSHTSECRET', { expiresIn: '60m' });
+    const refreshToken = await jwt.sign({ _id: user._id }, "MOGYSHTSECRET2", {
+      expiresIn: "1d",
+    });
 
-        // return { user, accessToken, refreshToken };
-    }
-    // }else{
-    //     throw new Error('No such user');
-    // }
+    user.refreshToken = refreshToken;
+    const userRoles = user.roles;
+    user.roles = userRoles;
+
+    await user.save();
+
+    return { user, accessToken, refreshToken, userRoles };
+
+    // let accessToken = jwt.sign({_id: user._id, username: user.username, roles: user.roles}, 'MOGYSHTSECRET', { expiresIn: '1m' });
+    // let refreshToken = await jwt.sign({ _id: user._id }, 'MOGYSHTSECRET2', { expiresIn: '1d' });
+
+    // user.refreshToken = refreshToken;
+
+    // await user.save();
+
+    // return { user, accessToken, refreshToken };
+  }
+  // }else{
+  //     throw new Error('No such user');
+  // }
 };
 
 exports.refresh = async (refreshToken) => {
-    const { _id } = jwt.verify(refreshToken, 'mnogoqkaparola2');
+  const { _id } = jwt.verify(refreshToken, "mnogoqkaparola2");
 
-    const user = await User.find({ _id, refreshToken });
+  const user = await User.find({ _id, refreshToken });
 
-    if (user) {
-            const accessToken = createAccessToken(user);
-            const refreshToken = createRefreshToken(user);
+  if (user) {
+    const accessToken = createAccessToken(user);
+    const refreshToken = createRefreshToken(user);
 
-        return { accessToken, refreshToken };
-    } else {
-        return null;
-    }
+    return { accessToken, refreshToken };
+  } else {
+    return null;
+  }
 };
 
 // module.exports = {
