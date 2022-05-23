13daef1478644bae3d4fe5a6416570c64cacbd5b
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-05-23 09:32:27 +0300

    Update voucherService

--- client/src/components/Services/voucherService.js
+++ client/src/components/Services/voucherService.js
@@ -1,111 +1,110 @@
 export const settings = {
-    host: ''
+  host: "",
 };
 
 async function request(url, options) {
+  try {
+    const response = await fetch(url, options);
+
+    if (response.ok === false) {
+      const error = await response.json();
+      throw new Error(error.message);
+    }
+
     try {
-        const response = await fetch(url, options);
+      const data = await response.json();
 
-        if (response.ok === false) {
-            const error = await response.json();
-            throw new Error(error.message);
-        }
-
-        try {
-            const data = await response.json();
-
-            return data;
-        } catch (error) {
-            return response;
-        }
-
+      return data;
     } catch (error) {
-        alert(error.message);
-        throw error
+      return response;
     }
-};
+  } catch (error) {
+    alert(error.message);
+    throw error;
+  }
+}
 
+function getOption(method = "get", body) {
+  const options = {
+    method,
+    headers: {
+      //'Content-Type': 'application/json',
+      //'X-Authorization': token
+      //'X-Parse-Application-Id': 'L3A2gZGIu7UpTKTKwO3yTGs4QS0j6JGEeR4qKv5U',
+      //'X-Parse-REST-API-Key': 'sNs8wSD5FQtaOMFoGdxg5A4pNT5EmbCtFNrHvvsX'
+    },
+  };
 
+  const token = sessionStorage.getItem("authToken");
 
-function getOption(method = 'get', body) {
-    
+  // if (token != null) {
+  //'X-Authorization' = token
+  //     options.headers['X-Parse-Session-Token'] = token;
+  // }
 
+  if (body) {
+    options.headers["Content-Type"] = "application/json";
+    options.headers["X-Authorization"] = token;
+    options.body = JSON.stringify(body);
+  }
 
-    const options = {
-        method,
-        headers: {
-            //'Content-Type': 'application/json',
-            //'X-Authorization': token
-            //'X-Parse-Application-Id': 'L3A2gZGIu7UpTKTKwO3yTGs4QS0j6JGEeR4qKv5U',
-            //'X-Parse-REST-API-Key': 'sNs8wSD5FQtaOMFoGdxg5A4pNT5EmbCtFNrHvvsX'
-        }
-    }
-
-    const token = sessionStorage.getItem('authToken');
-  
-    // if (token != null) {
-    //'X-Authorization' = token
-    //     options.headers['X-Parse-Session-Token'] = token;
-    // }
-
-    if (body) {
-        
-        options.headers['Content-Type'] = 'application/json';
-        options.headers['X-Authorization'] = token;
-        options.body = JSON.stringify(body);
-    }
-
-    return options;
-
-};
+  return options;
+}
 
 export async function get(url) {
-    return await request(url, getOption());
-};
+  return await request(url, getOption());
+}
 
 export async function post(url, data) {
-    return await request(url, getOption('post', data));
-};
+  return await request(url, getOption("post", data));
+}
 
 export async function put(url, data) {
-    return await request(url, getOption('put', data));
-};
+  return await request(url, getOption("put", data));
+}
 
 export async function del(url) {
-    return await request(url, getOption('delete'));
-};
+  return await request(url, getOption("delete"));
+}
 
 export async function login(username, password) {
-    const result = await post(settings.host + '/users/login', { username, password });
+  const result = await post(settings.host + "/users/login", {
+    username,
+    password,
+  });
 
-    sessionStorage.setItem('username', result.username);
-    sessionStorage.setItem('authToken', result.accessToken);
-    sessionStorage.setItem('refreshToken', result.refreshToken);
-    sessionStorage.setItem('userId', result._id);
-    sessionStorage.setItem('userRoles', result.userRoles);
+  sessionStorage.setItem("username", result.username);
+  sessionStorage.setItem("authToken", result.accessToken);
+  sessionStorage.setItem("refreshToken", result.refreshToken);
+  sessionStorage.setItem("userId", result._id);
+  sessionStorage.setItem("userRoles", result.userRoles);
 
-    return result;
-};
+  return result;
+}
 
 export async function register(username, password, roles) {
-    const result = await post(settings.host + '/users/register', { username, password, roles });
+  const result = await post(settings.host + "/users/register", {
+    username,
+    password,
+    roles,
+  });
 
-    // sessionStorage.setItem('username', result.username);
-    // sessionStorage.setItem('authToken', result.accessToken);
-    // sessionStorage.setItem('refreshToken', result.refreshToken);
-    // sessionStorage.setItem('userId', result._id);
+  // sessionStorage.setItem('username', result.username);
+  // sessionStorage.setItem('authToken', result.accessToken);
+  // sessionStorage.setItem('refreshToken', result.refreshToken);
+  // sessionStorage.setItem('userId', result._id);
 
-    return result;
-};
+  return result;
+}
 
 export async function logout(token) {
-    const result = await get(settings.host + '/users/logout');
+  const result = await get(settings.host + "/users/logout");
 
-    sessionStorage.removeItem('username');
-    sessionStorage.removeItem('authToken');
-    sessionStorage.removeItem('refreshToken');
-    sessionStorage.removeItem('userId');
-    sessionStorage.removeItem('userRoles');
+  sessionStorage.removeItem("username");
+  sessionStorage.removeItem("authToken");
+  sessionStorage.removeItem("refreshToken");
+  sessionStorage.removeItem("userId");
+  sessionStorage.removeItem("userRoles");
 
-    return result;
+  return result;
 }
