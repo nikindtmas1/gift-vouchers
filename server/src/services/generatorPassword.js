dd79987490805456c39a939e26a35b80b8171ba8
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-05-23 09:32:48 +0300

    Update generator pass

--- server/src/services/generatorPassword.js
+++ server/src/services/generatorPassword.js
@@ -2,5 +2,5 @@ const bcrypt = require('bcrypt');
 
 (async () => {
     const salt = await bcrypt.genSalt(10);
-    console.log(await bcrypt.hash("@NikiN240424", salt));
+    console.log(await bcrypt.hash("niki123456", salt));
 })();
