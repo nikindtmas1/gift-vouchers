0b42a21d5ee146f6a6cef0ef599ad4ba389ee48c
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-05-25 11:52:03 +0300

    Update Dashboard

--- client/src/components/Dashboard/Dashboard.js
+++ client/src/components/Dashboard/Dashboard.js
@@ -1,13 +1,13 @@
-import React from 'react'
+import React from "react";
 
 const Dashboard = () => {
   return (
     <div className="menu">
-    <div className="homeimg">
-      <img src="https://www.thehungrypeacock.com.au/wp-content/uploads/2020/04/gift-voucher-3-scaled.jpg" alt='' />
+      <div className="homeimg">
+        <img src="images/gift-voucher-3-scaled.jpg" alt="" />
+      </div>
     </div>
-    </div>
-  )
-}
+  );
+};
 
-export default Dashboard
+export default Dashboard;
