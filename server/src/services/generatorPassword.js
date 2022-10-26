052e2d7875188985de6a858d5692b229e6ca37be
Nikolai Nikolaev <niki_nik_74@abv.bg>
2022-10-26 15:55:39 +0300

    Added users and passwords

--- server/src/services/generatorPassword.js
+++ server/src/services/generatorPassword.js
@@ -4,3 +4,7 @@ const bcrypt = require('bcrypt');
     const salt = await bcrypt.genSalt(10);
     console.log(await bcrypt.hash("niki123456", salt));
 })();
+
+//admin - (usename: nikispa, password: niki123456)
+//editor - (username: sparec, password: spa123456)
+//visitor - (username: mickeymouse, password: miki123456)
